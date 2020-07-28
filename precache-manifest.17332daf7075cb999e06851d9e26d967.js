b8ab03c72b7bcf535a5b69394fd1aca80f546a06
JanisDeveloper <janis@developer.lv>
2020-07-28 13:42:08 +0300

    Updates

---
+++ precache-manifest.17332daf7075cb999e06851d9e26d967.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2bcd3b61f64fb1c32c9a18346ae9a06",
+    "url": "/react-test-app/index.html"
+  },
+  {
+    "revision": "574f5ccad2374889ab18",
+    "url": "/react-test-app/static/css/main.32205024.chunk.css"
+  },
+  {
+    "revision": "13be50a780c7d8897e3c",
+    "url": "/react-test-app/static/js/2.06d4cffc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-test-app/static/js/2.06d4cffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "574f5ccad2374889ab18",
+    "url": "/react-test-app/static/js/main.9686dd1c.chunk.js"
+  },
+  {
+    "revision": "211b7c40e7dec9fd4ac3",
+    "url": "/react-test-app/static/js/runtime-main.8ad3d07f.js"
+  },
+  {
+    "revision": "d8d764debed77879bbaa41d8ab374a3b",
+    "url": "/react-test-app/static/media/btn_delete.d8d764de.svg"
+  }
+]);
